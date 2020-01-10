e0ea63b572a4ca8e2d5e425e8852b68a4086d47d
Giri Kuncoro <girikuncoro@gmail.com>
2020-01-10 18:09:27 +0530

    Comment the info html by default and add bandung/yogyakarta places

--- poster/generator/places.js
+++ poster/generator/places.js
@@ -3,5 +3,13 @@ var places = {
   jakarta: {
     lat: -6.2088,
     lng: 106.8456
+  },
+  bandung: {
+    lat: -6.9175,
+    lng: 107.6191
+  },
+  yogyakarta: {
+    lat: -7.7956,
+    lng: 110.3695
   }
 };
